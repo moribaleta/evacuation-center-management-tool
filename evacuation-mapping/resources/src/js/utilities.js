b5c2a4cb6c53bc5491d8a6b4ab6b0071ebaeb9d6
gabriel mori baleta <moribaleta@gmail.com>
2020-09-03 17:47:05 +0800

    updated map maker disabled optimized map buggy

--- evacuation-mapping/resources/src/js/utilities.js
+++ evacuation-mapping/resources/src/js/utilities.js
@@ -15,18 +15,23 @@ function optimizeMap(road_map) {
     })
     console.log("road_map %o", road_map)
     console.log("coordinates %o", coordinates)
-    var coordinates2 = [...coordinates]
+    var coordinates2 = JSON.parse(JSON.stringify(coordinates))
     
+    var match = []
+
     coordinates.forEach((value_1, index_1) => {
         coordinates2.forEach((value_2, index_2) => {
             let distance = getdistance(
             value_1.lat, value_2.lat,
             value_1.lng, value_2.lng)
-            console.log("distance %o", distance)
-            if (distance < 0.03) {
+            //console.log("distance %o", distance)
+            if (distance < 0.03 &&
+                value_1.id != value_2.id &&
+                !sameParentID(value_1.id, value_2.id)) {
                 coordinates2[index_2].lat = value_1.lat
                 coordinates2[index_2].lng = value_1.lng
-                console.log("hey im low, val1: %o vs val2: %o", value_1, value_2)
+                match.push({value_1, value_2})
+                //console.log("hey im low, val1: %o vs val2: %o", value_1, value_2)
             }  
         })
     })
@@ -41,8 +46,16 @@ function optimizeMap(road_map) {
         })
         return path
     })
+
+    console.log("matches %o", match)
     
     return optimized
+}
+
+function sameParentID(id1, id2){
+    let parent1 = id1.split("-")[0]
+    let parent2 = id2.split("-")[0]
+    return parent1 == parent2
 }
 
 
@@ -69,4 +82,32 @@ function genID(length) {
     }
     return result;
  }
+
+
+ ///generates a file of json contains
+ function saveTextAsFile(text) {
+    const textToWrite = text
+    const textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+    const fileNameToSaveAs = "coordinates.json"//document.getElementById("").value;
+    let downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null)
+    {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    }
+    else
+    {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+    
+    downloadLink.click();
+}//saveTextAsFile
  
